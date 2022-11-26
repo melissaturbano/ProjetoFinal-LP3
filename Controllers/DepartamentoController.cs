00d8f1d127ade205e86a735de7ec21bdad6a618d
Melissa Turbano <melissaturbano@gmail.com>
2022-11-26 16:00:55 -0300

    conflito git

--- Controllers/DepartamentoController.cs
+++ Controllers/DepartamentoController.cs
@@ -43,5 +43,40 @@ public class DepartamentoController : Controller
         return View(departamento);
     }
 
+    //UPDATE
+    public IActionResult Update (int id)
+    {
+        Departamento departamento  = _context.Departamentos.Find(id);
+
+        if(departamento == null)
+        {
+            return NotFound();
+        }
+        return View(departamento);
+    }
+
+    [HttpPost]
+    public IActionResult Update
+    (int id, [FromForm] string nome, [FromForm] string sigla, [FromForm] string descricao, [FromForm] string bloco, [FromForm] string telefone, [FromForm] string email)
+    {
+        Departamento departamento = _context.Departamentos.Find(id);
+        if(departamento == null)
+        {
+            return NotFound();
+        }
+
+        departamento.Nome = nome;
+        departamento.Sigla = sigla;
+        departamento.Descricao = descricao;
+        departamento.Bloco = bloco;
+        departamento.Telefone = telefone;
+        departamento.Email = email;
+        
+        _context.Departamentos.Update(departamento);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
+
   
 }
