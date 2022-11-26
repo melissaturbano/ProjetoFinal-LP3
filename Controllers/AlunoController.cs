0420ef959232e2589a733cb3590f1f913dbac096
Melissa Turbano <melissaturbano@gmail.com>
2022-11-25 23:41:40 -0300

    adicionando Update de 'Aluno'

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -17,6 +17,7 @@ public class AlunoController : Controller
         return View(_context.Alunos);
      }
 
+   //SHOW
      public IActionResult Show(int id)
      {
         Aluno aluno = _context.Alunos.Find(id);
@@ -28,6 +29,7 @@ public class AlunoController : Controller
         return View(aluno);
      }
 
+   //DELETE
      public IActionResult Delete(int id)
     {
         Aluno aluno = _context.Alunos.Find(id);
@@ -39,6 +41,39 @@ public class AlunoController : Controller
         _context.Alunos.Remove(aluno);
         _context.SaveChanges();
         return View(aluno);
+    }
+
+    /*UPDATE*/
+    public IActionResult Update (int id)
+    {
+        Aluno aluno  = _context.Alunos.Find(id);
+
+        if(aluno == null)
+        {
+            return NotFound();
+        }
+        return View(aluno);
+    }
+
+    [HttpPost]
+    public IActionResult Update
+    (int id, [FromForm] string prontuario, [FromForm] string nome, [FromForm] string turma, [FromForm] string curso, [FromForm] string periodo)
+    {
+        Aluno aluno = _context.Alunos.Find(id);
+        if(aluno == null)
+        {
+            return NotFound();
+        }
+        aluno.Prontuario = prontuario;
+        aluno.Nome = nome;
+        aluno.Turma = turma;
+        aluno.Curso = curso;
+        aluno.Periodo = periodo;
+        
+        _context.Alunos.Update(aluno);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
     }
 
   
